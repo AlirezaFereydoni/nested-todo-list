84f439ca00bf7a120a991af3f14b99bc45337a25
Alireza Fereydoni <Aliirzw@gmail.com>
2024-05-06 23:37:14 +0330

    chore: loop through tasks and render them

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import { observer } from 'mobx-react';
-import useStores from './store/useStores';
 import { Task } from './components/task';
+import TaskStore from './store/tasks';
+import Styles from './app.module.css';
 
-// TODO - feel free to modify "App" content and develope the solution
+const renderTasks = tasks => {
+  return (
+    <>
+      {Object.values(tasks)
+        .sort((a, b) => a.priority - b.priority)
+        .map(item => (
+          <Task
+            key={item.id}
+            value={item.text}
+            onChange={e => TaskStore.editText(e.target.value, item.id)}
+            onAddSubTask={() => TaskStore.addTask('', item.id)}
+            onDelete={() => TaskStore.deleteTask(item.id)}
+            onHighPriority={() => TaskStore.editPriority(item.priority - 1, item.id)}
+            onLowPriority={() => TaskStore.editPriority(item.priority + 2, item.id)}
+          >
+            {renderTasks(item.subTasks)}
+          </Task>
+        ))}
+    </>
+  );
+};
+
 const App = observer(() => {
-  const { TasksStore } = useStores();
-
-  return <div className='App'>
-    <Task/>
-  </div>;
+  console.log(TaskStore.tasks);
+  return (
+    <div className={Styles.container}>
+      <h1>DigiExpress Nested TodoList</h1>
+      <button className={Styles['add-new']} onClick={() => TaskStore.addTask('')}>
+        Add New Task
+      </button>
+      {renderTasks(TaskStore.tasks)}
+    </div>
+  );
 });
 
 export default App;
